a99e06f9d677f7f36d29a63e8e988b5cec203f92
SergioML <sermali95@gmail.com>
2018-04-20 19:41:07 -0500

    Actualizaciones

--- scripts/create_df.py
+++ scripts/create_df.py
@@ -6,48 +6,47 @@ df = pd.read_csv('../data/clean/train_clean_v2.csv')
 print('Data train cargado')
 df_copy = df.copy()
 df_target = pd.read_csv('../data/clean/train_labels.csv')
-print("Targets cargado")
+print("Targets cargados")
 
-all_records = df_copy.ncodpers.value_counts().index
+ncodpers = df_copy.ncodpers.value_counts()
 
-first_records = {} #Primer registro de cada cantidad
+first_records = {}
 for i in range(17):
-    first_records[i+1] = df_copy.ncodpers.value_counts().index[np.unique(df_copy.ncodpers.value_counts().values, return_index=True)[1][i]]
+    first_records[i+1] = ncodpers.index[np.unique(ncodpers.values, return_index=True)[1][i]]
 
-l_records = {} #los indices según la cantidad que hay de cada uno
-for i in np.unique(df_copy.ncodpers.value_counts().values, return_index=True)[0]:
+records_per_amount = {}
+for i in np.unique(ncodpers.values, return_index=True)[0]:
     if i == 1:
-        l_records[i] = all_records[np.where(all_records==first_records[1])[0][0]:]
+        records_per_amount[i] = ncodpers.index[np.where(ncodpers.index == first_records[i])[0][0]:]
     else:
-        l_records[i] = all_records[np.where(all_records==first_records[i])[0][0]:np.where(all_records==first_records[i-1])[0][0]]
-
-only_record = df_copy.loc[df_copy['ncodpers'].isin(l_records[1])].sort_values(['ncodpers', 'fecha_dato']).index
+        records_per_amount[i] = ncodpers.index[np.where(ncodpers.index == first_records[i])[0][0]:np.where(ncodpers.index == first_records[i-1])[0][0]:]
+        
+only_record = df_copy.loc[df_copy['ncodpers'].isin(records_per_amount[1])].sort_values(['ncodpers', 'fecha_dato']).index
 df_copy.drop(only_record, inplace=True)
 
 group_ncodpers = df_copy.groupby('ncodpers')
-group_fecha = df_copy.groupby('fecha_dato')
+print(len(group_ncodpers.groups.keys()), 'ncodpers')
 
+start = time()
 df_new = pd.DataFrame()
 
-print("Nuevo dataset creado")
-
-for i in range(2, len(l_records)+1):
+for i in range(2, len(group_ncodpers.groups.keys())+1):
     start2 = time()
-    lista = l_records[i]
-    for code in lista:
-        df_aux = group_ncodpers.get_group(code) #DF con los registros de un código
-
-        df_aux1 = df_aux.iloc[:-1] #Se obtienen los registros menos el ltimo
-
-        df_aux_target = df_target.iloc[df_aux.index].iloc[1:] #Se obtienen los targets desde el segundo en adelante
+    for ncod in records_per_amount[i]:
+        df_aux = group_ncodpers.get_group(ncod) #DF con los registros de un código
+        df_aux1 = df_aux.iloc[:-1] #Se obtienen la info de los clientes menos del último mes
+        
+        df_aux_target = df_target.iloc[df_aux.index[1:]] #Se obtienen los productos desde el segundo mes en adelante
         df_aux_target.set_index(df_aux1.index, inplace=True) #Se actualizan los índices del anterior df con los del aux1
-
-        df_aux3 = pd.concat([df_aux1, df_aux_target], axis=1) #Se juntan ambos df resultantes
-
-        df_new = df_new.append(df_aux3) #Se concatenan entre sí
+        
+        df_new = df_new.append(pd.concat([df_aux1, df_aux_target], axis=1))
     end2 = time()
     partial_time = end2 - start2
 
     print("Con", i, "se demoró", partial_time, "segundos", (partial_time)/float(60), "minutos")
-
-df_new.to_csv("../data/clean/new_df.csv", index=False)
+    print("-----------------------------------------------------------------------------------")  
+    
+end = time()
+print("Tiempo total", (end-start)/3600, "hrs")
+#df_new.to_csv("../data/clean/new_df2.csv", index=False)
+#print("Nuevo dataset creado")
