9aa256acbb92557f5e63b18b5dc6b7a620fdf6f4
SergioML <sermali95@gmail.com>
2018-04-21 21:14:30 -0500

    Notebook con pruebas de las metricas

--- scripts/metrics.py
+++ scripts/metrics.py
@@ -45,6 +45,8 @@ x_test = x[10094948:]
 y_train = y[:10094948]
 y_test = y[10094948:]
 
+print("Negative is 0 and Positive is 1")
+
 for index, row in enumerate(df_targets.columns):
     rf = RandomForestClassifier()
     tn, fp, fn, tp, sensitivity, specifity = metrics(x_train, y_train[:, index], x_test, y_test[:, index], rf)
@@ -52,7 +54,7 @@ for index, row in enumerate(df_targets.columns):
     print('True positive:', tp)
     print('False positive:', fp)
     print('True positive rate or sensitivity:', sensitivity)
-    print('True negative:' tn)
+    print('True negative:', tn)
     print('False negative:', fn)
     print('True negative rate or specifity:', specifity)
     print()
